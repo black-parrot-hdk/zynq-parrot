f8185301cf517cbce01ce54744804d7a2cc3f0bf
taylor-bsg <prof.taylor@gmail.com>
2021-05-09 07:17:38 +0000

    example of decoder

---
+++ cosim/decoder-example/ps.cpp
@@ -0,0 +1,34 @@
+//
+// this is an example of "host code" that can either run in cosim or on the PS
+// we can use the same C host code and
+// the API we provide abstracts away the
+// communication plumbing differences.
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "bp_zynq_pl.h"
+
+int main(int argc, char **argv) {
+        bp_zynq_pl *zpl = new bp_zynq_pl(argc, argv);
+
+	// this program just communicates with a "loopback accelerator"
+	// that has 4 control registers that you can read and write
+	
+	int val1 = 0xDEADBEEF;
+	int val2 = 0xCAFEBABE;
+	int mask1 = 0xf;
+	int mask2 = 0xf;
+	
+	zpl->axil_write(0x0 + ADDR_BASE, val1, mask1);
+	zpl->axil_write(0x4 + ADDR_BASE, val2, mask2);
+
+	assert( (zpl->axil_read(0x0 + ADDR_BASE) == (val1)));
+	assert( (zpl->axil_read(0x4 + ADDR_BASE) == (val2)));
+	
+	zpl->done();
+
+	delete zpl;
+	exit(EXIT_SUCCESS);
+}
+
