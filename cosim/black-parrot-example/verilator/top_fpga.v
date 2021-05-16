c27c69d30e63883b4cce52b4921ea38ef01b40de
mbt <prof.taylor@gmail.com>
2021-05-16 12:31:44 -0700

    beautification complete

--- cosim/black-parrot-example/verilator/top_fpga.v
+++ cosim/black-parrot-example/verilator/top_fpga.v
@@ -116,27 +116,27 @@ module top_fpga
     );
 
    logic [2:0][C_S00_AXI_DATA_WIDTH-1:0]        csr_data_lo;
-   logic [C_S00_AXI_DATA_WIDTH-1:0]             out_fifo_data_li, in_fifo_data_lo;
-   logic                                        out_fifo_v_li, out_fifo_ready_lo;
-   logic                                        in_fifo_v_lo, in_fifo_yumi_li;
+   logic [C_S00_AXI_DATA_WIDTH-1:0]             pl_to_ps_fifo_data_li, ps_to_pl_fifo_data_lo;
+   logic                                        pl_to_ps_fifo_v_li, pl_to_ps_fifo_ready_lo;
+   logic                                        ps_to_pl_fifo_v_lo, ps_to_pl_fifo_yumi_li;
 
    // Instantiation of Axi Bus Interface S00_AXI
    bp_zynq_pl_shell #
      (
-       .C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
-       .C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
-       ) example_axi_v1_0_S00_AXI_inst
+      .C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+      .C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+      ) example_axi_v1_0_S00_AXI_inst
        (
         .csr_data_o(csr_data_lo)
         ,.csr_data_i()
 
-        ,.pl_to_ps_fifo_data_i (out_fifo_data_li)
-        ,.pl_to_ps_fifo_v_i    (out_fifo_v_li)
-        ,.pl_to_ps_fifo_ready_o(out_fifo_ready_lo)
+        ,.pl_to_ps_fifo_data_i (pl_to_ps_fifo_data_li)
+        ,.pl_to_ps_fifo_v_i    (pl_to_ps_fifo_v_li)
+        ,.pl_to_ps_fifo_ready_o(pl_to_ps_fifo_ready_lo)
 
-        ,.ps_to_pl_fifo_data_o (in_fifo_data_lo)
-        ,.ps_to_pl_fifo_v_o    (in_fifo_v_lo)
-        ,.ps_to_pl_fifo_yumi_i (in_fifo_yumi_li)
+        ,.ps_to_pl_fifo_data_o (ps_to_pl_fifo_data_lo)
+        ,.ps_to_pl_fifo_v_o    (ps_to_pl_fifo_v_lo)
+        ,.ps_to_pl_fifo_yumi_i (ps_to_pl_fifo_yumi_li)
 
         ,.S_AXI_ACLK   (s00_axi_aclk)
         ,.S_AXI_ARESETN(s00_axi_aresetn)
@@ -161,190 +161,188 @@ module top_fpga
         ,.S_AXI_RREADY (s00_axi_rready)
         );
 
-        // Add user logic here
+   // Add user logic here
 
-        `declare_bp_bedrock_mem_if(paddr_width_p, uce_mem_data_width_lp, lce_id_width_p, lce_assoc_p, uce);
-        bp_bedrock_uce_mem_msg_s io_cmd_lo, io_resp_li;
-        logic io_cmd_v_lo, io_cmd_ready_and_li;
-        logic io_resp_v_li, io_resp_yumi_lo;
+   `declare_bp_bedrock_mem_if(paddr_width_p, uce_mem_data_width_lp, lce_id_width_p, lce_assoc_p, uce);
 
-        `declare_bsg_cache_dma_pkt_s(caddr_width_p);
-        bsg_cache_dma_pkt_s dma_pkt_lo;
-        logic dma_pkt_v_lo, dma_pkt_yumi_li;
-        logic [l2_fill_width_p-1:0] dma_data_lo;
-        logic dma_data_v_lo, dma_data_yumi_li;
-        logic [l2_fill_width_p-1:0] dma_data_li;
-        logic dma_data_v_li, dma_data_ready_and_lo;
+   bp_bedrock_uce_mem_msg_s   io_cmd_lo, io_resp_li;
+   logic                      io_cmd_v_lo, io_cmd_ready_and_li;
+   logic                      io_resp_v_li, io_resp_yumi_lo;
 
-        logic [C_S01_AXI_ADDR_WIDTH-1:0] waddr_translated_lo, raddr_translated_lo;
-        always_comb
-          begin
-             if (s01_axi_awaddr < 32'hA0000000)
-               waddr_translated_lo = s01_axi_awaddr - csr_data_lo[0];
-             else
-               waddr_translated_lo = s01_axi_awaddr - csr_data_lo[1];
-          end
+   `declare_bsg_cache_dma_pkt_s(caddr_width_p);
+   bsg_cache_dma_pkt_s dma_pkt_lo;
+   logic                       dma_pkt_v_lo, dma_pkt_yumi_li;
+   logic [l2_fill_width_p-1:0] dma_data_lo;
+   logic                       dma_data_v_lo, dma_data_yumi_li;
+   logic [l2_fill_width_p-1:0] dma_data_li;
+   logic                       dma_data_v_li, dma_data_ready_and_lo;
 
-        always_comb
-          begin
-             if (s01_axi_araddr < 32'hA0000000)
-               raddr_translated_lo = s01_axi_araddr - csr_data_lo[0];
-             else
-               raddr_translated_lo = s01_axi_araddr - csr_data_lo[1];
-          end
+   logic [C_S01_AXI_ADDR_WIDTH-1:0] waddr_translated_lo, raddr_translated_lo;
+   always_comb
+     begin
+        if (s01_axi_awaddr < 32'hA0000000)
+          waddr_translated_lo = s01_axi_awaddr - csr_data_lo[0];
+        else
+          waddr_translated_lo = s01_axi_awaddr - csr_data_lo[1];
+     end
 
-      bp_to_axi_decoder #
-        (.bp_params_p(bp_params_p))
-      bp_out_data
-        (.clk_i(s01_axi_aclk)
-         ,.reset_i(~s01_axi_aresetn)
+   always_comb
+     begin
+        if (s01_axi_araddr < 32'hA0000000)
+          raddr_translated_lo = s01_axi_araddr - csr_data_lo[0];
+        else
+          raddr_translated_lo = s01_axi_araddr - csr_data_lo[1];
+     end
 
-         ,.io_cmd_i(io_cmd_lo)
-         ,.io_cmd_v_i(io_cmd_v_lo)
-         ,.io_cmd_ready_and_o(io_cmd_ready_and_li)
+   bp_to_axi_decoder #
+     (.bp_params_p(bp_params_p))
+   bp_out_data
+     (.clk_i   ( s01_axi_aclk)
+      ,.reset_i(~s01_axi_aresetn)
 
-         ,.io_resp_o(io_resp_li)
-         ,.io_resp_v_o(io_resp_v_li)
-         ,.io_resp_yumi_i(io_resp_yumi_lo)
+      ,.io_cmd_i          (io_cmd_lo)
+      ,.io_cmd_v_i        (io_cmd_v_lo)
+      ,.io_cmd_ready_and_o(io_cmd_ready_and_li)
 
-         ,.data_o(out_fifo_data_li)
-         ,.v_o(out_fifo_v_li)
-         ,.ready_i(out_fifo_ready_lo)
-         );
+      ,.io_resp_o     (io_resp_li)
+      ,.io_resp_v_o   (io_resp_v_li)
+      ,.io_resp_yumi_i(io_resp_yumi_lo)
 
-      localparam axi_id_width_p = 6;
-      localparam axi_addr_width_p = 33;
-      localparam axi_data_width_p = 64;
-      localparam axi_strb_width_p = axi_data_width_p >> 3;
-      localparam axi_burst_len_p = 8;
+      ,.data_o (pl_to_ps_fifo_data_li)
+      ,.v_o    (pl_to_ps_fifo_v_li)
+      ,.ready_i(pl_to_ps_fifo_ready_lo)
+      );
 
-      wire [32:0] axi_awaddr;
-      wire [32:0] axi_araddr;
-      assign m00_axi_awaddr = axi_awaddr - 32'h80000000 + csr_data_lo[2];
-      assign m00_axi_araddr = axi_araddr - 32'h80000000 + csr_data_lo[2];
+   localparam axi_id_width_p = 6;
+   localparam axi_addr_width_p = 33;
+   localparam axi_data_width_p = 64;
+   localparam axi_strb_width_p = axi_data_width_p >> 3;
+   localparam axi_burst_len_p = 8;
 
-      bp_unicore_axi_sim #
-        (.bp_params_p(bp_params_p))
-      blackparrot
-        (.clk_i(s01_axi_aclk)
-         ,.reset_i(~s01_axi_aresetn)
+   wire [32:0] axi_awaddr;
+   wire [32:0] axi_araddr;
+   assign m00_axi_awaddr = axi_awaddr - 32'h80000000 + csr_data_lo[2];
+   assign m00_axi_araddr = axi_araddr - 32'h80000000 + csr_data_lo[2];
 
-         ,.io_cmd_o          (io_cmd_lo)
-         ,.io_cmd_v_o        (io_cmd_v_lo)
-         ,.io_cmd_ready_and_i(io_cmd_ready_and_li)
+   bp_unicore_axi_sim #
+     (.bp_params_p(bp_params_p))
+   blackparrot
+     (.clk_i(s01_axi_aclk)
+      ,.reset_i(~s01_axi_aresetn)
 
-         ,.io_resp_i         (io_resp_li)
-         ,.io_resp_v_i       (io_resp_v_li)
-         ,.io_resp_yumi_o    (io_resp_yumi_lo)
+      ,.io_cmd_o          (io_cmd_lo)
+      ,.io_cmd_v_o        (io_cmd_v_lo)
+      ,.io_cmd_ready_and_i(io_cmd_ready_and_li)
 
-         ,.s_axi_lite_awaddr_i (waddr_translated_lo)
-         ,.s_axi_lite_awprot_i (s01_axi_awprot)
-         ,.s_axi_lite_awvalid_i(s01_axi_awvalid)
-         ,.s_axi_lite_awready_o(s01_axi_awready)
+      ,.io_resp_i         (io_resp_li)
+      ,.io_resp_v_i       (io_resp_v_li)
+      ,.io_resp_yumi_o    (io_resp_yumi_lo)
 
-         ,.s_axi_lite_wdata_i  (s01_axi_wdata)
-         ,.s_axi_lite_wstrb_i  (s01_axi_wstrb)
-         ,.s_axi_lite_wvalid_i (s01_axi_wvalid)
-         ,.s_axi_lite_wready_o (s01_axi_wready)
+      ,.s_axi_lite_awaddr_i (waddr_translated_lo)
+      ,.s_axi_lite_awprot_i (s01_axi_awprot)
+      ,.s_axi_lite_awvalid_i(s01_axi_awvalid)
+      ,.s_axi_lite_awready_o(s01_axi_awready)
 
-         ,.s_axi_lite_bresp_o  (s01_axi_bresp)
-         ,.s_axi_lite_bvalid_o (s01_axi_bvalid)
-         ,.s_axi_lite_bready_i (s01_axi_bready)
+      ,.s_axi_lite_wdata_i  (s01_axi_wdata)
+      ,.s_axi_lite_wstrb_i  (s01_axi_wstrb)
+      ,.s_axi_lite_wvalid_i (s01_axi_wvalid)
+      ,.s_axi_lite_wready_o (s01_axi_wready)
 
-         ,.s_axi_lite_araddr_i (raddr_translated_lo)
-         ,.s_axi_lite_arprot_i (s01_axi_arprot)
-         ,.s_axi_lite_arvalid_i(s01_axi_arvalid)
-         ,.s_axi_lite_arready_o(s01_axi_arready)
+      ,.s_axi_lite_bresp_o  (s01_axi_bresp)
+      ,.s_axi_lite_bvalid_o (s01_axi_bvalid)
+      ,.s_axi_lite_bready_i (s01_axi_bready)
 
-         ,.s_axi_lite_rdata_o  (s01_axi_rdata)
-         ,.s_axi_lite_rresp_o  (s01_axi_rresp)
-         ,.s_axi_lite_rvalid_o (s01_axi_rvalid)
-         ,.s_axi_lite_rready_i (s01_axi_rready)
+      ,.s_axi_lite_araddr_i (raddr_translated_lo)
+      ,.s_axi_lite_arprot_i (s01_axi_arprot)
+      ,.s_axi_lite_arvalid_i(s01_axi_arvalid)
+      ,.s_axi_lite_arready_o(s01_axi_arready)
 
-         ,.dma_pkt_o     (dma_pkt_lo)
-         ,.dma_pkt_v_o   (dma_pkt_v_lo)
-         ,.dma_pkt_yumi_i(dma_pkt_yumi_li)
+      ,.s_axi_lite_rdata_o  (s01_axi_rdata)
+      ,.s_axi_lite_rresp_o  (s01_axi_rresp)
+      ,.s_axi_lite_rvalid_o (s01_axi_rvalid)
+      ,.s_axi_lite_rready_i (s01_axi_rready)
 
-         ,.dma_data_i          (dma_data_li)
-         ,.dma_data_v_i        (dma_data_v_li)
-         ,.dma_data_ready_and_o(dma_data_ready_and_lo)
+      ,.dma_pkt_o     (dma_pkt_lo)
+      ,.dma_pkt_v_o   (dma_pkt_v_lo)
+      ,.dma_pkt_yumi_i(dma_pkt_yumi_li)
 
-         ,.dma_data_o     (dma_data_lo)
-         ,.dma_data_v_o   (dma_data_v_lo)
-         ,.dma_data_yumi_i(dma_data_yumi_li)
-         );
+      ,.dma_data_i          (dma_data_li)
+      ,.dma_data_v_i        (dma_data_v_li)
+      ,.dma_data_ready_and_o(dma_data_ready_and_lo)
 
-        assign m00_axi_awqos = '0;
-        assign m00_axi_arqos = '0;
-        assign m00_axi_wid = m00_axi_awid;
+      ,.dma_data_o     (dma_data_lo)
+      ,.dma_data_v_o   (dma_data_v_lo)
+      ,.dma_data_yumi_i(dma_data_yumi_li)
+      );
 
-      bsg_cache_to_axi #
-        (.addr_width_p(caddr_width_p)
-         ,.data_width_p(l2_fill_width_p)
-         ,.block_size_in_words_p(l2_block_size_in_fill_p)
-         ,.num_cache_p(1)
-         ,.axi_id_width_p  (axi_id_width_p)
-         ,.axi_addr_width_p(axi_addr_width_p)
-         ,.axi_data_width_p(axi_data_width_p)
-         ,.axi_burst_len_p (axi_burst_len_p)
-         )
-      cache2axi
-        (.clk_i   (m00_axi_aclk)
-         ,.reset_i(~m00_axi_aresetn)
+   assign m00_axi_awqos = '0;
+   assign m00_axi_arqos = '0;
+   assign m00_axi_wid = m00_axi_awid;
 
-         ,.dma_pkt_i     (dma_pkt_lo)
-         ,.dma_pkt_v_i   (dma_pkt_v_lo)
-         ,.dma_pkt_yumi_o(dma_pkt_yumi_li)
+   bsg_cache_to_axi #
+     (.addr_width_p(caddr_width_p)
+      ,.data_width_p(l2_fill_width_p)
+      ,.block_size_in_words_p(l2_block_size_in_fill_p)
+      ,.num_cache_p(1)
+      ,.axi_id_width_p  (axi_id_width_p)
+      ,.axi_addr_width_p(axi_addr_width_p)
+      ,.axi_data_width_p(axi_data_width_p)
+      ,.axi_burst_len_p (axi_burst_len_p)
+      )
+   cache2axi
+     (.clk_i   (m00_axi_aclk)
+      ,.reset_i(~m00_axi_aresetn)
 
-         ,.dma_data_o      (dma_data_li)
-         ,.dma_data_v_o    (dma_data_v_li)
-         ,.dma_data_ready_i(dma_data_ready_and_lo)
+      ,.dma_pkt_i       (dma_pkt_lo)
+      ,.dma_pkt_v_i     (dma_pkt_v_lo)
+      ,.dma_pkt_yumi_o  (dma_pkt_yumi_li)
 
-         ,.dma_data_i     (dma_data_lo)
-         ,.dma_data_v_i   (dma_data_v_lo)
-         ,.dma_data_yumi_o(dma_data_yumi_li)
+      ,.dma_data_o      (dma_data_li)
+      ,.dma_data_v_o    (dma_data_v_li)
+      ,.dma_data_ready_i(dma_data_ready_and_lo)
 
-         ,.axi_awid_o  (m00_axi_awid)
-         ,.axi_awaddr_o(axi_awaddr)
-         ,.axi_awlen_o (m00_axi_awlen)
-         ,.axi_awsize_o(m00_axi_awsize)
-         ,.axi_awburst_o(m00_axi_awburst)
-         ,.axi_awcache_o(m00_axi_awcache)
-         ,.axi_awprot_o (m00_axi_awprot)
-         ,.axi_awlock_o (m00_axi_awlock)
-         ,.axi_awvalid_o(m00_axi_awvalid)
-         ,.axi_awready_i(m00_axi_awready)
+      ,.dma_data_i      (dma_data_lo)
+      ,.dma_data_v_i    (dma_data_v_lo)
+      ,.dma_data_yumi_o (dma_data_yumi_li)
 
-         ,.axi_wdata_o  (m00_axi_wdata)
-         ,.axi_wstrb_o  (m00_axi_wstrb)
-         ,.axi_wlast_o  (m00_axi_wlast)
-         ,.axi_wvalid_o (m00_axi_wvalid)
-         ,.axi_wready_i (m00_axi_wready)
+      ,.axi_awid_o   (m00_axi_awid)
+      ,.axi_awaddr_o (axi_awaddr)
+      ,.axi_awlen_o  (m00_axi_awlen)
+      ,.axi_awsize_o (m00_axi_awsize)
+      ,.axi_awburst_o(m00_axi_awburst)
+      ,.axi_awcache_o(m00_axi_awcache)
+      ,.axi_awprot_o (m00_axi_awprot)
+      ,.axi_awlock_o (m00_axi_awlock)
+      ,.axi_awvalid_o(m00_axi_awvalid)
+      ,.axi_awready_i(m00_axi_awready)
 
-         ,.axi_bid_i    (m00_axi_bid)
-         ,.axi_bresp_i  (m00_axi_bresp)
-         ,.axi_bvalid_i (m00_axi_bvalid)
-         ,.axi_bready_o (m00_axi_bready)
+      ,.axi_wdata_o  (m00_axi_wdata)
+      ,.axi_wstrb_o  (m00_axi_wstrb)
+      ,.axi_wlast_o  (m00_axi_wlast)
+      ,.axi_wvalid_o (m00_axi_wvalid)
+      ,.axi_wready_i (m00_axi_wready)
 
-         ,.axi_arid_o   (m00_axi_arid)
-         ,.axi_araddr_o (axi_araddr)
-         ,.axi_arlen_o  (m00_axi_arlen)
-         ,.axi_arsize_o (m00_axi_arsize)
-         ,.axi_arburst_o(m00_axi_arburst)
-         ,.axi_arcache_o(m00_axi_arcache)
-         ,.axi_arprot_o (m00_axi_arprot)
-         ,.axi_arlock_o (m00_axi_arlock)
-         ,.axi_arvalid_o(m00_axi_arvalid)
-         ,.axi_arready_i(m00_axi_arready)
+      ,.axi_bid_i    (m00_axi_bid)
+      ,.axi_bresp_i  (m00_axi_bresp)
+      ,.axi_bvalid_i (m00_axi_bvalid)
+      ,.axi_bready_o (m00_axi_bready)
 
-         ,.axi_rid_i    (m00_axi_rid)
-         ,.axi_rdata_i  (m00_axi_rdata)
-         ,.axi_rresp_i  (m00_axi_rresp)
-         ,.axi_rlast_i  (m00_axi_rlast)
-         ,.axi_rvalid_i (m00_axi_rvalid)
-         ,.axi_rready_o (m00_axi_rready)
-         );
+      ,.axi_arid_o   (m00_axi_arid)
+      ,.axi_araddr_o (axi_araddr)
+      ,.axi_arlen_o  (m00_axi_arlen)
+      ,.axi_arsize_o (m00_axi_arsize)
+      ,.axi_arburst_o(m00_axi_arburst)
+      ,.axi_arcache_o(m00_axi_arcache)
+      ,.axi_arprot_o (m00_axi_arprot)
+      ,.axi_arlock_o (m00_axi_arlock)
+      ,.axi_arvalid_o(m00_axi_arvalid)
+      ,.axi_arready_i(m00_axi_arready)
 
-        // User logic ends
-
+      ,.axi_rid_i    (m00_axi_rid)
+      ,.axi_rdata_i  (m00_axi_rdata)
+      ,.axi_rresp_i  (m00_axi_rresp)
+      ,.axi_rlast_i  (m00_axi_rlast)
+      ,.axi_rvalid_i (m00_axi_rvalid)
+      ,.axi_rready_o (m00_axi_rready)
+      );
 endmodule
