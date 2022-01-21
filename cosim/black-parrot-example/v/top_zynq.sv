0e1eb02969cddc969dd79d99810439daf5d2ee33
Dan Petrisko <petrisko@cs.washington.edu>
2022-01-21 08:56:53 -0800

    Unswizzling in top

--- cosim/black-parrot-example/v/top_zynq.sv
+++ cosim/black-parrot-example/v/top_zynq.sv
@@ -9,7 +9,7 @@ module top_zynq
  import bp_common_pkg::*;
  import bp_be_pkg::*;
  import bp_me_pkg::*;
- #(parameter bp_params_e bp_params_p = e_bp_unicore_cfg
+ #(parameter bp_params_e bp_params_p = e_bp_default_cfg
    `declare_bp_proc_params(bp_params_p)
    `declare_bp_bedrock_mem_if_widths(paddr_width_p, did_width_p, lce_id_width_p, lce_assoc_p, uce)
 
