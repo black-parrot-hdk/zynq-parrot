8d66df8f469e7a965276af8b86d5bc6bf5fd7b91
Yuan-Mao <ymchueh@gmail.com>
2022-02-14 13:02:29 -0800

    Move files

--- cosim/black-parrot-example/fpga/run_all.tcl
+++ cosim/black-parrot-example/fpga/run_all.tcl
@@ -1,7 +1,7 @@
-source ../syn/bp_ethernet.xdc
-source ../syn/io_pin.xdc
-source ../../import/eth_1g_core/syn/vivado/axis_async_fifo.tcl
-source ../../import/eth_1g_core/syn/vivado/rgmii_phy_if.tcl
-source ../../import/eth_1g_core/syn/vivado/eth_mac_1g_rgmii_fifo.tcl
-source ../../import/eth_1g_core/syn/vivado/eth_mac_1g_rgmii.tcl
-source ../../import/eth_1g_core/syn/vivado/zedboard.tcl
+source syn/bp_ethernet.xdc
+source syn/io_pin.xdc
+source ../../import/eth_1g_core/syn/zedboard/axis_async_fifo.tcl
+source ../../import/eth_1g_core/syn/zedboard/rgmii_phy_if.tcl
+source ../../import/eth_1g_core/syn/zedboard/eth_mac_1g_rgmii_fifo.tcl
+source ../../import/eth_1g_core/syn/zedboard/eth_mac_1g_rgmii.tcl
+source ../../import/eth_1g_core/syn/zedboard/zedboard.tcl
