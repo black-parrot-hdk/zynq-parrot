91ebb3adb3a16a116ca28243e90bf2cf4bf6e4d1
Yuan-Mao <ymchueh@gmail.com>
2022-02-18 00:18:48 -0800

    Remove unnecessary FIFO

--- cosim/black-parrot-example/v/ethernet_axil_wrapper.sv
+++ cosim/black-parrot-example/v/ethernet_axil_wrapper.sv
@@ -67,13 +67,6 @@ module ethernet_axil_wrapper
     logic [size_width_lp-1:0]     cmd_data_size_lo;
     logic [axil_data_width_p-1:0] cmd_wdata_lo;
 
-    logic [axil_data_width_p-1:0] resp_fifo_li;
-    logic                         resp_fifo_v_li;
-    logic                         resp_fifo_ready_lo;
-    logic                         resp_fifo_v_lo;
-    logic [axil_data_width_p-1:0] resp_fifo_lo;
-    logic                         resp_fifo_ready_and_li;
-    logic                         resp_fifo_yumi_li;
     logic                         disable_r;
 
     logic                         rx_interrupt_pending_lo;
@@ -83,6 +76,16 @@ module ethernet_axil_wrapper
     wire read_en_li = cmd_v_lo & ~cmd_wr_en_lo;
     logic read_en_lo;
 
+    logic resp_v_li, resp_ready_and_lo;
+    logic [axil_data_width_p-1:0] resp_rdata_li;
+
+    bsg_dff_reset #(.width_p(1))
+     resp_reg (
+      .clk_i(clk_i)
+      ,.reset_i(reset_i)
+      ,.data_i(read_en_li | write_en_li)
+      ,.data_o(resp_v_li));
+
     // Allow only 1 outstanding request
     bsg_dff_reset_set_clear #(
       .width_p(1)
@@ -91,33 +94,10 @@ module ethernet_axil_wrapper
       .clk_i(clk_i)
       ,.reset_i(reset_i)
       ,.set_i(cmd_v_lo & cmd_ready_and_li)
-      ,.clear_i(resp_fifo_yumi_li)
+      ,.clear_i(resp_v_li & resp_ready_and_lo)
       ,.data_o(disable_r)
     );
-    assign resp_fifo_yumi_li = resp_fifo_v_lo & resp_fifo_ready_and_li;
     assign cmd_ready_and_li = ~disable_r;
-    assign resp_fifo_v_li = read_en_lo | write_en_li;
-
-    // this tracks both read and write
-    bsg_one_fifo #(
-       .width_p(axil_data_width_p)
-    ) resp_fifo (
-       .clk_i(clk_i)
-      ,.reset_i(reset_i)
-      ,.v_i(resp_fifo_v_li)
-      ,.ready_o(resp_fifo_ready_lo)
-      ,.data_i(resp_fifo_li)
-      ,.v_o(resp_fifo_v_lo)
-      ,.data_o(resp_fifo_lo)
-      ,.yumi_i(resp_fifo_yumi_li)
-    );
-
-    //synopsys translate_off
-    always_ff @(posedge clk_i) begin
-      if(~reset_i & (resp_fifo_v_li & ~resp_fifo_ready_lo))
-        $display("ethernet_controller_wrapper.sv: read data dropped");
-    end
-    //synopsys translate_on
 
     axil_client_adaptor #(
        .axil_data_width_p(axil_data_width_p)
@@ -133,9 +113,9 @@ module ethernet_axil_wrapper
       ,.cmd_data_size_o(cmd_data_size_lo)
       ,.cmd_wdata_o(cmd_wdata_lo)
 
-      ,.resp_v_i(resp_fifo_v_lo)
-      ,.resp_ready_and_o(resp_fifo_ready_and_li)
-      ,.resp_rdata_i(resp_fifo_lo)
+      ,.resp_v_i(resp_v_li)
+      ,.resp_ready_and_o(resp_ready_and_lo)
+      ,.resp_rdata_i(resp_rdata_li)
 
       ,.s_axil_awaddr_i
       ,.s_axil_awprot_i
@@ -176,8 +156,7 @@ module ethernet_axil_wrapper
        ,.read_en_i(read_en_li)
        ,.op_size_i(cmd_data_size_lo)
        ,.write_data_i(cmd_wdata_lo)
-       ,.read_data_o(resp_fifo_li) // sync read
-       ,.read_data_v_o(read_en_lo)
+       ,.read_data_o(resp_rdata_li) // sync read
 
        ,.rx_interrupt_pending_o(rx_interrupt_pending_lo)
        ,.tx_interrupt_pending_o(tx_interrupt_pending_lo)
