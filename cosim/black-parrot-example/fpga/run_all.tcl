7a265015894fe543f485381f9e5c4af09b49be05
Yuan-Mao <ymchueh@gmail.com>
2022-03-02 17:30:10 -0800

    Fix import path

--- cosim/black-parrot-example/fpga/run_all.tcl
+++ cosim/black-parrot-example/fpga/run_all.tcl
@@ -1,7 +1,7 @@
 source syn/bp_ethernet.xdc
 source syn/io_pin.xdc
-source ../../import/eth_1g_core/syn/zedboard/axis_async_fifo.tcl
-source ../../import/eth_1g_core/syn/zedboard/rgmii_phy_if.tcl
-source ../../import/eth_1g_core/syn/zedboard/eth_mac_1g_rgmii_fifo.tcl
-source ../../import/eth_1g_core/syn/zedboard/eth_mac_1g_rgmii.tcl
-source ../../import/eth_1g_core/syn/zedboard/zedboard.tcl
+source ../../import/black-parrot-subsystems/ethernet_controller/syn/zedboard/axis_async_fifo.tcl
+source ../../import/black-parrot-subsystems/ethernet_controller/syn/zedboard/rgmii_phy_if.tcl
+source ../../import/black-parrot-subsystems/ethernet_controller/syn/zedboard/eth_mac_1g_rgmii_fifo.tcl
+source ../../import/black-parrot-subsystems/ethernet_controller/syn/zedboard/eth_mac_1g_rgmii.tcl
+source ../../import/black-parrot-subsystems/ethernet_controller/syn/zedboard/zedboard.tcl
