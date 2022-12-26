c5acfe809d3d8ea6d9562ee7285ac47847fa3d0e
Michael Taylor <prof.taylor@gmail.com>
2022-12-26 14:00:38 -0800

    increase throughput

--- cosim/shell-example/fpga/vivado-create-block.2022.1.tcl
+++ cosim/shell-example/fpga/vivado-create-block.2022.1.tcl
@@ -5,6 +5,9 @@ create_bd_design "shell_bd_1"
 update_compile_order -fileset sources_1
 startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
+
+# THIS SETS THE CLOCK FREQUENCY
+set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ {178}] [get_bd_cells processing_system7_0]
 endgroup
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
 open_bd_design ${project_name}.srcs/sources_1/bd/shell_bd_1/shell_bd_1.bd}
