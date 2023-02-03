5337358b7ac3a5511bb9770cfa74f4f140073b11
Yuan-Mao <ymchueh@gmail.com>
2023-02-03 00:22:22 -0800

    Refactoring c++ code for bsg tag bitbang

--- cosim/black-parrot-example/ps.cpp
+++ cosim/black-parrot-example/ps.cpp
@@ -158,18 +158,21 @@ extern "C" void cosim_main(char *argstr) {
               zpl->axil_read(GP0_RD_CSR_DRAM_INITED + gp0_addr_base),
               val = zpl->axil_read(GP0_RD_CSR_DRAM_BASE + gp0_addr_base));
 #ifdef BITBANG_ENABLE
+  std::unique_ptr<bsg_tag_bitbang> tag;
+  tag = std::make_unique<bsg_tag_bitbang>((zpl->axi_gp0).get(), bp_zynq_pl::tick, GP0_WR_CSR_BITBANG);
   // Reset the bsg tag master
-  zpl->tag->bsg_tag_reset(zpl, GP0_RD_CSR_BITBANG + gp0_addr_base);
+  tag->bsg_tag_reset_master();
   // Reset bsg client0
-  zpl->tag->bsg_tag_packet_write(zpl, GP0_RD_CSR_BITBANG + gp0_addr_base, NUM_RESET, 1, 0, 0, -1U);
+  tag->bsg_tag_packet_write(NUM_RESET, 1, 0, 0, -1U);
   // Set bsg client0 to 1 (assert BP reset)
-  zpl->tag->bsg_tag_packet_write(zpl, GP0_RD_CSR_BITBANG + gp0_addr_base, NUM_RESET, 1, 1, 0, 0x1);
+  tag->bsg_tag_packet_write(NUM_RESET, 1, 1, 0, 0x1);
   // Set bsg client0 to 0 (deassert BP reset)
-  zpl->tag->bsg_tag_packet_write(zpl, GP0_RD_CSR_BITBANG + gp0_addr_base, NUM_RESET, 1, 1, 0, 0x0);
+  tag->bsg_tag_packet_write(NUM_RESET, 1, 1, 0, 0x0);
 
   // We need at least 4 additional toggles for data to propagate through
   for(int i = 0;i < 4;i++)
-    zpl->tag->bsg_tag_bit_write(zpl, GP0_RD_CSR_BITBANG + gp0_addr_base, 0x0);
+    tag->bsg_tag_bit_write(0);
+
 #endif
 
   bsg_pr_info("ps.cpp: attempting to write and read register 0x8\n");
