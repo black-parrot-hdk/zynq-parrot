9553555e68d255df09e03bf972c8005d40b49e98
Yuan-Mao <ymchueh@gmail.com>
2023-02-23 20:17:31 -0800

    Add missing HP0_ENABLE

--- cosim/include/vcs/bp_zynq_pl.h
+++ cosim/include/vcs/bp_zynq_pl.h
@@ -164,6 +164,7 @@ public:
     }
 #endif
 
+#ifdef HP0_ENABLE
     if (axi_hp0->p_awvalid && (axi_hp0->p_awaddr >= SCRATCHPAD_BASE) && (axi_hp0->p_awaddr < SCRATCHPAD_BASE+SCRATCHPAD_SIZE)) {
       axi_hp0->axil_write_helper((axil_device *)scratchpad.get(), tick);
     } else if (axi_hp0->p_arvalid && (axi_hp0->p_araddr >= SCRATCHPAD_BASE) && (axi_hp0->p_araddr < SCRATCHPAD_BASE+SCRATCHPAD_SIZE)) {
@@ -176,6 +177,7 @@ public:
       bsg_pr_err("  bp_zynq_pl: Unsupported AXI device read at [%x]\n", araddr);
     }
   }
+#endif
 };
 
 #endif
