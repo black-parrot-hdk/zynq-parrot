14b9c69354d913f3f6600113bc26118d9e5dd9a7
Dan Petrisko <petrisko@cs.washington.edu>
2024-10-09 21:32:41 -0700

    Bump subsystems

--- cosim/tcl/bd/axi_plic_bd.tcl
+++ cosim/tcl/bd/axi_plic_bd.tcl
@@ -24,43 +24,43 @@ proc vivado_create_ip { args } {
     set COSIM_VSRC_DIR $::env(COSIM_VSRC_DIR)
 
     lappend dir_list "${BASEJUMP_STL_DIR}/bsg_misc"
-    lappend dir_list "${BASEJUMP_STL_DIR}/bsg_tag"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/prim_assert.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/prim_assert_dummy_macros.svh"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/prim_assert_sec_cm.svh"
+    lappend file_list "${BASEJUMP_STL_DIR}/bsg_misc/bsg_defines.sv"
 
     lappend file_list "${BASEJUMP_STL_DIR}/bsg_axi/bsg_axi_pkg.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_tag/bsg_tag_pkg.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/hard/ultrascale_plus/bsg_async/bsg_launch_sync_sync.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_dataflow/bsg_one_fifo.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_misc/bsg_counter_clear_up.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_misc/bsg_dff.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_misc/bsg_dff_en.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_misc/bsg_dff_reset.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_misc/bsg_mux2_gatestack.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_tag/bsg_tag_master_decentralized.sv"
-    lappend file_list "${BASEJUMP_STL_DIR}/bsg_tag/bsg_tag_client.sv"
-    lappend file_list "${CURR_VSRC_DIR}/zynq_pkg.sv"
     lappend file_list "${BLACKPARROT_SUB_DIR}/axi/v/bsg_axil_fifo_master.sv"
-    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/bsg_axil_watchdog.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/axi/v/bsg_axil_fifo_client.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/bsg_axil_plic.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/bsg_irq_to_axil.sv"
+
+    lappend file_list "${BASEJUMP_STL_DIR}/bsg_dataflow/bsg_one_fifo.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/top_pkg.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/rv_plic_reg_pkg.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/rv_plic.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/rv_plic_gateway.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/rv_plic_target.sv"
+    lappend file_list "${BLACKPARROT_SUB_DIR}/zynq/v/gen/rv_plic_reg_top.sv"
 
     add_files -norecurse ${file_list}
     set_property file_type SystemVerilog [get_files ${file_list}]
-    add_files -norecurse "${BLACKPARROT_SUB_DIR}/zynq/v/watchdog_top.v"
+    add_files -norecurse "${BLACKPARROT_SUB_DIR}/zynq/v/plic_top.v"
     set_property include_dirs ${dir_list} [get_fileset sources_1]
-    set_property top watchdog_top [get_fileset sources_1]
+    set_property top plic_top [get_fileset sources_1]
     update_compile_order -fileset sources_1
 
-    create_bd_cell -type module -reference watchdog_top -name top
+    create_bd_cell -type module -reference plic_top -name top
     create_bd_port -dir I -type clk -freq_hz ${aclk_freq_hz} aclk
     create_bd_port -dir I -type rst aresetn
-    create_bd_port -dir I -type data tag_clk
-    create_bd_port -dir I -type data tag_data
     connect_bd_net [get_bd_ports aclk] [get_bd_pins top/aclk]
     connect_bd_net [get_bd_ports aresetn] [get_bd_pins top/aresetn]
     make_bd_intf_pins_external -name "M_AXI" [get_bd_intf_pins top/m_axil]
-    set_property CONFIG.ASSOCIATED_BUSIF {M_AXI} [get_bd_ports aclk]
+    make_bd_intf_pins_external -name "S_AXI" [get_bd_intf_pins top/s_axil]
+    set_property CONFIG.ASSOCIATED_BUSIF {M_AXI:S_AXI} [get_bd_ports aclk]
     set_property CONFIG.ASSOCIATED_RESET {aresetn} [get_bd_ports aclk]
     set_property CONFIG.PROTOCOL {AXI4LITE} [get_bd_intf_ports M_AXI]
-    connect_bd_net [get_bd_ports tag_clk] [get_bd_pins top/tag_clk]
-    connect_bd_net [get_bd_ports tag_data] [get_bd_pins top/tag_data]
+    set_property CONFIG.PROTOCOL {AXI4LITE} [get_bd_intf_ports S_AXI]
 
     # Need to increase aperture size because of vivado bug
     assign_bd_address
