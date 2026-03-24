f69161ecb85bcbc0539bafb519794f71fc044602
MahelaEkanayake <mahelaekanayake.aurelius@gmail.com>
2026-03-24 13:00:11 +0530

    Add initial trace_capture module for commit-stage tracing (WIP)

---
+++ cosim/black-parrot-example/v/trace_capture.sv
@@ -0,0 +1,23 @@
+module trace_capture
+  #(parameter width_p = 64)
+  (
+    input  logic                         clk_i
+  , input  logic                         reset_i
+
+  // Simplified commit interface (to be refined)
+  , input  logic                         commit_v_i
+  , input  logic [31:0]                  pc_i
+  , input  logic [31:0]                  instr_i
+  , input  logic [31:0]                  npc_i
+  , input  logic                         exception_i
+
+  // Trace output
+  , output logic                         trace_v_o
+  , output logic [31:0]                  trace_pc_o
+  );
+
+  // Only capture architecturally committed instructions
+  assign trace_v_o  = commit_v_i;
+  assign trace_pc_o = pc_i;
+
+endmodule
